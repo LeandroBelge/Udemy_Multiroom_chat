7c7d38e91472968d699dc6df21ae7f418ac3df75
LeandroBelge <leandrobelge@gmail.com>
2018-04-12 16:54:52 -0300

    Abrindo a conexão WebSocket

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ var server = app.listen(80, function(){
     console.log('Servidor online');
 })
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+/*Criar a conexão po websocket*/
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    })
+});
